1f9f524bc082dc7abe78c583f44b6386473328d7
admin <nour.majed1234@gmail.com>
2023-02-04 18:02:05 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 const init = () => {
-  
+    const form = document.querySelector('form')
+    form.addEventListener('submit', (e) => {
+        e.preventDefault()
+        const input = document.querySelector('input#searchByID');
+        
+        fetch(`${window.location.origin}/movies/${input.value}`)
+        .then(response => response.json())
+        .then(data => {
+            const title = document.querySelector('#movieDetails h4')
+            const summary = document.querySelector('#movieDetails p')
+
+            title.innerText = data.title
+            summary.innerText = data.summary
+            console.log(title,summary)
+        })
+  })
 }
 
 document.addEventListener('DOMContentLoaded', init);
